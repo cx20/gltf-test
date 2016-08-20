d13bc95775997b80b02acfcc3c68b105a376bcc0
cx20 <y-shimano@hondarer-soft.com>
2016-08-20 21:49:26 +0900

    GLBoost + glTF Loader examples

---
+++ examples/glboost/Box/index.js
@@ -0,0 +1,45 @@
+var canvas = document.getElementById("world");
+
+var glBoostContext = new GLBoost.GLBoostMiddleContext(canvas);
+
+var renderer = glBoostContext.createRenderer({clearColor: {red: 1.0, green: 0.5, blue: 0.5, alpha: 1}});
+
+var scene = glBoostContext.createScene();
+
+var pointLight = glBoostContext.createPointLight(new GLBoost.Vector3(1.0, 1.0, 1.0));
+pointLight.translate = new GLBoost.Vector3(10, 10, 10);
+scene.addChild(pointLight);
+
+var camera = glBoostContext.createPerspectiveCamera({
+    eye: new GLBoost.Vector3(0.0, 1.0, 10),
+    center: new GLBoost.Vector3(0.0, 1.0, 0.0),
+    up: new GLBoost.Vector3(0.0, 1.0, 0.0)
+}, {
+    fovy: 45.0,
+    aspect: 1.0,
+    zNear: 0.1,
+    zFar: 300.0
+});
+scene.addChild(camera);
+
+var glTFLoader = GLBoost.GLTFLoader.getInstance();
+var promise = glTFLoader.loadGLTF(glBoostContext, '../../../sampleModels/Box/glTF-Embedded/Box.gltf', 1, null);
+promise.then(function(mesh) {
+    console.log(mesh);
+    scene.addChild(mesh);
+    window.mesh = mesh;
+    
+    var expression = glBoostContext.createExpressionAndRenderPasses(1);
+    expression.renderPasses[0].scene = scene;
+    expression.prepareToRender();
+   
+    var render = function() {
+        renderer.clearCanvas();
+        renderer.draw(expression);
+        var rotateMatrix = GLBoost.Matrix33.rotateY(-1.0);
+        var rotatedVector = rotateMatrix.multiplyVector(camera.eye);
+        camera.eye = rotatedVector;
+        requestAnimationFrame(render);
+    };
+    render();
+});
