dfed6b27f4fb32a44b2131a321e8f9b1e83653df
cx20 <y-shimano@hondarer-soft.com>
2016-08-21 00:44:32 +0900

    xeoEngine + glTF Loader examples

---
+++ examples/xeoengine/RiggedSimple/index.js
@@ -0,0 +1,28 @@
+// Load glTF
+var model = new XEO.Model({
+    src: "../../../sampleModels/RiggedSimple/glTF-Embedded/RiggedSimple.gltf"
+});
+
+var view = model.scene.camera.view;
+view.eye = [
+    0.0,
+    0.0,
+    -3.0
+];
+view.look = [
+    0.0,
+    0.0,
+    0.0
+];
+view.up = [
+    0.0,
+    1.0,
+    0.0
+];
+
+new XEO.CameraControl();
+model.scene.on("tick",
+    function () {
+        view.rotateEyeY(1.0);
+        //view.rotateEyeX(1.0);
+    });
