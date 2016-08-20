62fbdb53d962857369297cfcb739f35c87280648
cx20 <y-shimano@hondarer-soft.com>
2016-08-20 21:22:23 +0900

    Babylon.js + glTF Loader examples

--- examples/babylonjs/Box/index.js
+++ examples/babylonjs/Box/index.js
@@ -1,60 +1,35 @@
-var gltf = null;
+var createScene = function(engine) {
 
-init();
-animate();
-  
-function init() {
-    width = window.innerWidth;
-    height = window.innerHeight;
-    
-    scene = new THREE.Scene();
-  
-    var ambient = new THREE.AmbientLight( 0x101030 );
-    scene.add( ambient );
+    var scene = new BABYLON.Scene(engine);
+    var mesh;
+    scene.clearColor = new BABYLON.Color3(1, 1, 1);
 
-    var directionalLight = new THREE.DirectionalLight( 0xffeedd );
-    directionalLight.position.set( 0, 0, 1 );
-    scene.add( directionalLight );
+    // Box.gltf
+    BABYLON.SceneLoader.Load("../../../sampleModels/Box/", "glTF-Embedded/Box.gltf", engine, function (newScene) {
 
+        scene = newScene;
+        mesh = scene.meshes[0];
 
-    camera = new THREE.PerspectiveCamera( 75, width / height, 1, 2000 );
-    camera.position.z = 300;
+        camera = new BABYLON.ArcRotateCamera("camera", 0, 0, 5, BABYLON.Vector3.Zero(), scene);
+        camera.attachControl(canvas, false, false);
+        camera.panningSensibility = 50.0;
+        camera.angularSensibility = 500;
+        scene.activeCamera = camera;
 
-    var manager = new THREE.LoadingManager();
-    manager.onProgress = function ( item, loaded, total ) {
-        console.log( item, loaded, total );
-    };
-
-    var loader = new THREE.glTFLoader();
-
-    var url = "../../../sampleModels/Box/glTF-Embedded/Box.gltf"; // box.gltf
-    loader.load(url, function (data) {
-        var object = data.scene;
-        camera.position.copy(new THREE.Vector3(0, 2, 3));
-        scene.add(object);
+        var rad = 0.0;
+        engine.runRenderLoop(function () {
+            scene.render();
+            rad += Math.PI * 1.0 / 180.0;
+            // quaternion
+            mesh.rotate(BABYLON.Axis.X, Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
+            mesh.rotate(BABYLON.Axis.Y, Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
+            mesh.rotate(BABYLON.Axis.Z, Math.PI * 1.0 / 180.0, BABYLON.Space.LOCAL);
+            scene.render();
+        });
     });
-
-    var axis = new THREE.AxisHelper(1000);   
-    scene.add(axis);
-
-    renderer = new THREE.WebGLRenderer();
-
-    controls = new THREE.OrbitControls( camera, renderer.domElement );
-    controls.userPan = false;
-    controls.userPanSpeed = 0.0;
-    controls.maxDistance = 5000.0;
-    controls.maxPolarAngle = Math.PI * 0.495;
-    controls.autoRotate = true;     //true:Ž©“®‰ñ“]‚·‚é,false:Ž©“®‰ñ“]‚µ‚È‚¢
-    controls.autoRotateSpeed = -10.0;    //Ž©“®‰ñ“]‚·‚éŽž‚Ì‘¬“x
-
-    renderer.setSize( width, height );
-    document.body.appendChild( renderer.domElement );
-  
+    return scene;
 }
-  
-function animate() {
-    requestAnimationFrame( animate );
-    THREE.glTFShaders.update(scene, camera);
-    renderer.render( scene, camera );
-    controls.update();
-}
+
+var canvas = document.querySelector("#c");
+var engine = new BABYLON.Engine(canvas, true);
+var scene = createScene(engine);
