a0cf1bcaec3bf439ec1a21257a27cd952a35b95f
Andreas Plesch <andreasplesch@users.noreply.github.com>
2017-02-24 09:58:28 -0500

    Update index.js

--- examples/x3dom/index.js
+++ examples/x3dom/index.js
@@ -19,7 +19,7 @@ document.onload = function () {
         vp.attr({fieldofview: "0.263245"});
         vp.attr({position: "207.615 53.3281 51.6212"});
         vp.attr({orientation: "-0.5035214059784457 0.8384312102757996 0.20856485647622058 0.9177268588403985"});
-        vp.attr({centerofrotation: "-159.20 -17.02 -3.21"});
+        vp.attr({centerofrotation: "159.20 17.02 3.21"});
     }
     shape.append("<ExternalShape id='exshape' url='../../" + modelInfo.category + "/" + modelInfo.path + "' />");
 }
