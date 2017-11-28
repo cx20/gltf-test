92b27dbc4ac3f8f8f455d148910af95fc8073648
cx20 <y-shimano@hondarer-soft.com>
2017-11-29 03:48:20 +0900

    Update glTF 2.0 sample models

--- tutorialModels/pbr-model-index.js
+++ tutorialModels/pbr-model-index.js
@@ -8,6 +8,7 @@ TutorialPbrModelIndex.List = [
     {category:'tutorialModels', name:'Lantern', scale:0.06},
     {category:'tutorialModels', name:'WaterBottle', scale:10.0},
     {category:'tutorialModels', name:'DamagedHelmet', scale:1.0},
+    {category:'tutorialModels', name:'BoomBoxWithAxes', scale:80.0},
 ];
 
 TutorialPbrModelIndex.HasGifScreenshot = [ // List of only models that have *.gif screenshots (as opposed to *.png)
