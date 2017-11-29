f2d77ec9eb9c4ebb2abc04eb473ae17f19674a26
cx20 <y-shimano@hondarer-soft.com>
2017-11-29 22:45:41 +0900

    Update status for glTF 2.0

--- tutorialModels/model-index.js
+++ tutorialModels/model-index.js
@@ -10,8 +10,8 @@ TutorialModelIndex.List = [
     {category:'tutorialModels', name:'AnimatedMorphSphere', scale:1.0},
     {category:'tutorialModels', name:'SimpleMaterial', scale:1.0},
     {category:'tutorialModels', name:'SimpleMeshes', scale:1.0},
-    {category:'tutorialModels', name:'AdvancedMaterial', scale:1.0},
-    {category:'tutorialModels', name:'SimpleOpacity', scale:1.0},
+    //{category:'tutorialModels', name:'AdvancedMaterial', scale:1.0},
+    //{category:'tutorialModels', name:'SimpleOpacity', scale:1.0},
     {category:'tutorialModels', name:'SimpleTexture', scale:1.0},
     {category:'tutorialModels', name:'Cameras', scale:1.0},
     {category:'tutorialModels', name:'SimpleSkin', scale:1.0}
