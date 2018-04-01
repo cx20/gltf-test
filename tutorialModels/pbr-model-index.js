8cae477cd61924e3dd65f1940bd490b0c3592ca4
cx20 <y-shimano@hondarer-soft.com>
2018-04-01 17:54:56 +0900

    Update glTF 2.0 sample models

--- tutorialModels/pbr-model-index.js
+++ tutorialModels/pbr-model-index.js
@@ -6,7 +6,7 @@ TutorialPbrModelIndex.List = [
     {category:'tutorialModels', name:'BoomBox', scale:80.0},
     {category:'tutorialModels', name:'Corset', scale:25.0},
     {category:'tutorialModels', name:'DamagedHelmet', scale:1.0},
-    {category:'tutorialModels', name:'FlightHelmet', scale:0.5},
+    {category:'tutorialModels', name:'FlightHelmet', scale:5.0},
     {category:'tutorialModels', name:'Lantern', scale:0.06},
     {category:'tutorialModels', name:'WaterBottle', scale:10.0},
 ];
