c9c62013452ff8fc050f0ed881ce39d4651b3073
cx20 <y-shimano@hondarer-soft.com>
2018-04-21 13:44:33 +0900

    Update glTF 2.0 sample models

--- sampleModels/model-index.js
+++ sampleModels/model-index.js
@@ -19,7 +19,8 @@ ModelIndex.List = [
     {category:'sampleModels', name:'CesiumMan', scale:1.0},
     {category:'sampleModels', name:'Monster', scale:0.05},
     {category:'sampleModels', name:'BrainStem', scale:1.0},
-    {category:'sampleModels', name:'VC', scale:0.2}
+    {category:'sampleModels', name:'VC', scale:0.2},
+    {category:'sampleModels', name:'WalkingLady', scale:1.0}
 ];
 
 ModelIndex.HasGifScreenshot = [ // List of only models that have *.gif screenshots (as opposed to *.png)
