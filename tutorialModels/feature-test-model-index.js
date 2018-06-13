58def15deb028265143260b8e805f66d457640be
cx20 <y-shimano@hondarer-soft.com>
2018-06-14 00:27:31 +0900

    Added glTF 2.0 sample models https://github.com/06wj/glTF-Sample-Models/tree/master/2.0/MultiUVTest

--- tutorialModels/feature-test-model-index.js
+++ tutorialModels/feature-test-model-index.js
@@ -4,6 +4,7 @@ TutorialFeatureTestModelIndex.List = [
     {category:'tutorialModels', name:'AlphaBlendModeTest', scale:0.5},
     {category:'tutorialModels', name:'BoomBoxWithAxes', scale:80.0},
     {category:'tutorialModels', name:'MetalRoughSpheres', scale:0.2},
+    {category:'tutorialModels', name:'MultiUVTest', scale:1.0},
     {category:'tutorialModels', name:'NormalTangentTest', scale:1.0},
     {category:'tutorialModels', name:'NormalTangentMirrorTest', scale:1.0},
     {category:'tutorialModels', name:'OrientationTest', scale:0.2},
@@ -14,6 +15,7 @@ TutorialFeatureTestModelIndex.List = [
 
 TutorialFeatureTestModelIndex.HasJpgScreenshot = [ // List of only models that have *.jpg screenshots
     'BoomBoxWithAxes',
+    'MultiUVTest',
 ];
 
 TutorialFeatureTestModelIndex.getScreenshot = function(name) {
