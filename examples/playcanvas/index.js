73431dfccfa70cb753c615079c8b8f77a55d4375
cx20 <y-shimano@hondarer-soft.com>
2018-09-27 23:22:35 +0900

    Update example for PlayCanvas and glTF Loader

--- examples/playcanvas/index.js
+++ examples/playcanvas/index.js
@@ -44,7 +44,7 @@ app.root.addChild(camera);
 camera.setLocalPosition(0, 0, 1);
 
 // make the camera interactive
-app.assets.loadFromUrl('../../libs/playcanvas/v1.8.0-dev/orbit-camera.js', 'script', function (err, asset) {
+app.assets.loadFromUrl('../../libs/playcanvas/v1.9.0-dev/orbit-camera.js', 'script', function (err, asset) {
     camera.script.create('orbitCamera', {
         attributes: {
             inertiaFactor: 0,
@@ -107,13 +107,13 @@ function loadScript(src) {
 
 function init(){
     if (true) {//typeof WebAssembly !== 'object') {
-        loadScript('../../libs/playcanvas/v1.8.0-dev/draco_decoder.js').then(function () {
+        loadScript('../../libs/playcanvas/v1.9.0-dev/draco_decoder.js').then(function () {
             decoderModule = DracoDecoderModule();
             onLoad();
         });
     } else {
-        loadScript('../../libs/playcanvas/v1.8.0-dev/draco_wasm_wrapper.js').then(function () {
-            fetch('../../libs/playcanvas/v1.8.0-dev//draco_decoder.wasm').then(function (response) {
+        loadScript('../../libs/playcanvas/v1.9.0-dev/draco_wasm_wrapper.js').then(function () {
+            fetch('../../libs/playcanvas/v1.9.0-dev//draco_decoder.wasm').then(function (response) {
                 response.arrayBuffer().then(function (arrayBuffer) {
                     decoderModule = DracoDecoderModule({ wasmBinary: arrayBuffer });
                     onLoad();
