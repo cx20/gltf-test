8043526db9d5ccc389d4a5ed84e7e44e13bba0b2
cx20 <y-shimano@hondarer-soft.com>
2019-02-24 01:52:07 +0900

    Update library for Ashes and glTF Loader

--- examples/ashes3d/index.js
+++ examples/ashes3d/index.js
@@ -19,64 +19,54 @@ if (!modelInfo) {
     throw new Error('Model not specified or not found in list.');
 }
 
-var scale = modelInfo.scale;
-//var path = "../../sampleModels/" + modelInfo.path;
-var path = "../../" + modelInfo.category + "/" + modelInfo.path;
+let { Asset, EntityMgr, Camera, vec3, quat, Screen, OrbitControl, MeshRenderer, Filter, Shader, Material, QuadMesh } = Ashes;
+let CDN = 'https://but0n.github.io/Ashes/'
+
+let scale = modelInfo.scale;
+let path = "../../" + modelInfo.category + "/" + modelInfo.path;
 if(modelInfo.url) {
     path = modelInfo.url;
 }
-var base = path.substr(0, path.lastIndexOf("/") + 1);
-var file = path.substr(path.lastIndexOf("/") + 1);
+let base = path.substr(0, path.lastIndexOf("/") + 1);
+let file = path.substr(path.lastIndexOf("/") + 1);
 
 async function run() {
-    Ashes.Asset.taskObserve = (finished, total) => {
-        let p = finished/total;
-    }
     let gltf = path;
-    let yoffset = 0;
+    let yoffset = -0.1;
 
-    let screen = new Ashes.Screen('#screen');
-    let skybox = await Ashes.Asset.loadCubemap('https://but0n.github.io/Ashes/res/envmap/GoldenGateBridge2/');
+    let screen = new Screen('#screen');
+    screen.bgColor = [0.2,0.2,0.2,1];
+    let skybox = await Asset.loadCubemap(CDN + 'res/envmap/GoldenGateBridge2/');
 
-    let scene = Ashes.EntityMgr.create('root');
-    //let gltfroot = await Ashes.Asset.loadGLTF(gltf, screen, 'res/envmap/GoldenGateBridge2/');
-    let gltfroot = await Ashes.Asset.loadGLTF(gltf, screen, skybox);
-    scene.appendChild(gltfroot);
-    //progressBar.parentElement.style.display = 'none';
-    let root = gltfroot.components.Transform;
-    if(yoffset)
-        root.translate[1] = yoffset;
-    if(scale)
-        root.scale[0] = root.scale[1] = root.scale[2] = scale;
+    let scene = EntityMgr.create('root');
 
-
-    let mainCamera = Ashes.EntityMgr.create('camera');
+    // Create a camera entity
+    let mainCamera = EntityMgr.create('camera');
     let cameraTrans = mainCamera.components.Transform;
-    let cam = Ashes.EntityMgr.addComponent(mainCamera, new Ashes.Camera(screen.width / screen.height));
-    Ashes.vec3.set(cameraTrans.translate, 0, 0, 5);
+    let cam = EntityMgr.addComponent(mainCamera, new Camera(screen.width / screen.height));
+
+    //vec3.set(cameraTrans.translate, 0, 0, 0.1); // set a little bit z offset to allow you orbit around
+    vec3.set(cameraTrans.translate, 0, 0, 2); // set a little bit z offset to allow you orbit around
 
     scene.appendChild(mainCamera);
-    //let control = new Ashes.OrbitControl(screen, mainCamera);
-    Ashes.EntityMgr.addComponent(mainCamera, new Ashes.OrbitControl(screen, mainCamera));
+    EntityMgr.addComponent(mainCamera, new OrbitControl(screen, mainCamera));
 
-    // IBL test
-    let testmat = await Ashes.Asset.LoadMaterial('test');
-    // let brdfLUT = await Ashes.Asset.loadImage('https://raw.githubusercontent.com/KhronosGroup/glTF-WebGL-PBR/master/textures/brdfLUT.png');
-    // let map = await Ashes.Asset.loadCubemap('https://but0n.github.io/Ashes/res/envmap/GoldenGateBridge2/');
-
-    // Ashes.Material.setTexture(testmat, 'brdfLUT', new Ashes.Texture(brdfLUT, { minFilter: screen.gl.LINEAR }));
-    // Ashes.Material.setTexture(testmat, 'baseColorTexture', Ashes.MeshRendererSystem);
-
-    // let quad = Ashes.EntityMgr.create('test-quad');
-    // let qmesh = new Ashes.QuadMesh();
-    // let quadMR = new Ashes.MeshRenderer(screen, qmesh, testmat);
-    // Ashes.EntityMgr.addComponent(quad, quadMR);
-    // console.log(quadMR);
-    // quad.components.Transform.translate[0] = 2;
-
-    // scene.appendChild(quad);
-
+    // Load gltf scene
+    let gltfroot = await Asset.loadGLTF(gltf, screen, skybox);
+    scene.appendChild(gltfroot);
+    
+    let root = gltfroot.components.Transform;
+    root.translate[1] = yoffset || 0;
+    vec3.scale(root.scale, root.scale, scale);
+    
     document.querySelector('body').appendChild(scene);
+
+    // find a node, attach camera
+    let movingTarget = document.querySelectorAll('[data-name=Correction__MovingCamera]')[0];
+    if (movingTarget !== undefined ) {
+        movingTarget.appendChild(mainCamera);
+        EntityMgr.hasNewMember = true;
+    }
 }
 
 run();
