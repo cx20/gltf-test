58258add39ca5c12446bf206c76fe9810827fe5a
cx20 <y-shimano@hondarer-soft.com>
2019-10-31 07:14:35 +0900

    Update library for Three.js and glTF Loader

--- examples/threejs/index.js
+++ examples/threejs/index.js
@@ -82,7 +82,7 @@ function init() {
     loader.setCrossOrigin( 'anonymous' );
 
     var dracoLoader = new THREE.DRACOLoader();
-    dracoLoader.setDecoderPath( '../../libs/three.js/r109dev/draco/gltf/' );
+    dracoLoader.setDecoderPath( '../../libs/three.js/r110dev/draco/gltf/' );
     loader.setDRACOLoader( dracoLoader );
 
     let scale = modelInfo.scale;
