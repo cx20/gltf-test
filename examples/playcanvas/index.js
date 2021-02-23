6ae971fdb47af4f74f0ac846b1b4ba90de7a4f4b
cx20 <y-shimano@hondarer-soft.com>
2021-02-24 00:28:32 +0900

    Update PlayCanvas v1.39.0 to v1.39.3

--- examples/playcanvas/index.js
+++ examples/playcanvas/index.js
@@ -145,7 +145,7 @@ var Viewer = function (canvas) {
 
     // load orbit script
     app.assets.loadFromUrl(
-        "../../libs/playcanvas/v1.39.0/orbit-camera.js",
+        "../../libs/playcanvas/v1.39.3/orbit-camera.js",
         "script",
         function (err, asset) {
             // setup orbit script component
@@ -432,9 +432,9 @@ function startViewer() {
 
 function main(){
     if (wasmSupported()) {
-        loadWasmModuleAsync('DracoDecoderModule', '../../libs/playcanvas/v1.39.0/draco.wasm.js', '../../libs/playcanvas/v1.39.0/draco.wasm.wasm', startViewer);
+        loadWasmModuleAsync('DracoDecoderModule', '../../libs/playcanvas/v1.39.3/draco.wasm.js', '../../libs/playcanvas/v1.39.3/draco.wasm.wasm', startViewer);
     } else {
-        loadWasmModuleAsync('DracoDecoderModule', '../../libs/playcanvas/v1.39.0/draco.js', '', startViewer);
+        loadWasmModuleAsync('DracoDecoderModule', '../../libs/playcanvas/v1.39.3/draco.js', '', startViewer);
     }
 }
 
