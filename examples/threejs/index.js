4e026150484c0fee2b4ad1ad236c2028631b2828
cx20 <y-shimano@hondarer-soft.com>
2022-10-29 06:28:30 +0900

    Update three.js r145 to r146

--- examples/threejs/index.js
+++ examples/threejs/index.js
@@ -134,10 +134,10 @@ function init() {
     const loader = new GLTFLoader(manager);
     loader.setCrossOrigin( 'anonymous' );
 
-    const dracoLoader = new DRACOLoader().setDecoderPath( '../../libs/three.js/r145/examples/js/libs/draco/gltf/' );
+    const dracoLoader = new DRACOLoader().setDecoderPath( '../../libs/three.js/r146/examples/js/libs/draco/gltf/' );
     loader.setDRACOLoader( dracoLoader );
 
-    const ktx2Loader = new KTX2Loader().setTranscoderPath( '../../libs/three.js/r145/examples/js/libs/basis/' );
+    const ktx2Loader = new KTX2Loader().setTranscoderPath( '../../libs/three.js/r146/examples/js/libs/basis/' );
     ktx2Loader.detectSupport( renderer );
     loader.setKTX2Loader( ktx2Loader );
 
