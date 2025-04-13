24de16b5d1f39e7011a9108edc5dd08397a14563
cx20 <y-shimano@hondarer-soft.com>
2025-04-14 00:32:02 +0900

    Update PlayCanvas v2.5.1 to v2.6.2

--- examples/playcanvas/index.js
+++ examples/playcanvas/index.js
@@ -28,7 +28,7 @@ if (!modelInfo) {
     throw new Error('Model not specified or not found in list.');
 }
 
-const pcRoot = '../../libs/playcanvas/v2.5.1';
+const pcRoot = '../../libs/playcanvas/v2.6.2';
 
 const DEFAULT_NAME = "[default]";
 
